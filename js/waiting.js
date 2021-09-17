a5361cfbaee0a483ba26a7723e3b269442a41fce
Nicole <nicky260899@gmail.com>
2021-09-17 15:45:57 -0600

    fixes del footer, instrucciones en español y game info arreglo de diseño del footer en general se agrega logo de la u

--- js/waiting.js
+++ js/waiting.js
@@ -8,13 +8,12 @@ function showHelp() {
   const htmlHelp = `<div class="help-content">
   <h3>Rules</h3>
   <ul>
-  <li>The player will have a set of cards assigned for them to find.</li>
+  <li>The player will have a set of cards assigned for him or her to find.</li>
   <li>The cards assigned to the player will be sentences describing a card on the board.</li> 
-  <li>The player will need to associate the sentence (his card) with an image in the game board.</li>
+  <li>The player will need to associate the sentence (his card) with an image on the game board.</li>
   <li>Once the player has found all of the cards matching the sentences, a message indicating that he has won will appear and a new game will be created (with the same game settings).</li>
   <li>If the winning button is selected, the player will need to press it once he has found all the cards. The winning message will not appear by itself.</li>
   </ul>
-  <h3>Credits</h3>
 
   <div>`;
   Swal.fire({
@@ -24,7 +23,56 @@ function showHelp() {
     showCloseButton: true,
     focusConfirm: true,
     confirmButtonText:
+      'Done',
+    confirmButtonColor: baseColorOne,
+  });
+}
+
+function showHelpSpanish() {
+  const htmlHelp = `<div class="help-content">
+  <h3>Reglas</h3>
+  <ul>
+  <li> El jugador tendrá un set de cartas asignado para que las encuentre. </li>
+   <li> Las cartas asignadas al jugador serán frases que describen una imagen en el tablero. </li>
+   <li> El jugador deberá asociar la oración (su carta) con una imagen en el tablero del juego. </li>
+   <li> Una vez que el jugador haya encontrado todas las cartas que coincidan con las oraciones, aparecerá un mensaje que indica que ha ganado y se creará un nuevo juego (con la misma configuración del juego que estaba jugando). </li>
+   <li> Si se selecciona el botón ganador ("winning button"), el jugador deberá presionarlo una vez que haya encontrado todas las cartas. El mensaje ganador no aparecerá solo. </li>
+  </ul>
+
+  <div>`;
+  Swal.fire({
+    title: 'Ayuda',
+    icon: 'info',
+    html: htmlHelp,
+    showCloseButton: true,
+    focusConfirm: true,
+    confirmButtonText:
       'Listo',
+    confirmButtonColor: baseColorOne,
+  });
+}
+
+
+function showCredits() {
+  const htmlHelp = `<div class="help-content">
+  <h3>Game information</h3>
+  <ul>
+  <li> Creator: Nicole Castillo Arguedas</li>
+  <li> Student project for TCU-501</li>
+  <li> Designed for second grade students.</li> 
+  <li> Theme: Fabulous Flora and Fauna, Unit 5</li>
+  <li> Images designed by Sophia Borge</li>
+  </ul>
+
+  <div>`;
+  Swal.fire({
+    title: 'Credits',
+    icon: 'info',
+    html: htmlHelp,
+    showCloseButton: true,
+    focusConfirm: true,
+    confirmButtonText:
+      'Done',
     confirmButtonColor: baseColorOne,
   });
 }
@@ -180,8 +228,14 @@ function showInputsError(errors, titleText) {
     leave.addEventListener('click', () => { exitConfirm(); });
   
     // Agregar evento al botón de ayuda
-    const help = document.getElementById('help-img-button');
+    const help = document.getElementById('help-tag');
     help.addEventListener('click', showHelp);
+
+    const helpSpa = document.getElementById('help-tag-spa');
+    helpSpa.addEventListener('click', showHelpSpanish);
+
+    const credits = document.getElementById('credits');
+    credits.addEventListener('click', showCredits);
 
     const initGame = document.getElementById('play');
     initGame.addEventListener('click', startGame);
